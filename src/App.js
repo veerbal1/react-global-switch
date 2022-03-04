18589e84132af364a624e34d8fa96d32b122d491
veerbal1 <veerbal1singh1@gmail.com>
2022-03-04 19:36:47 +0530

    Test seemed pass for context api core fetch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-
-const foo = async () => {
-  await setTimeout(() => {
-    console.log(5, 'React webpack template');
-  }, 5000);
-};
+import Context, { MyContext } from './Context';
 
 /**
  * @return {JSX.Element}
  */
 function App() {
-  foo();
-  return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>My App</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Context>
+                <h1>Global State Testing</h1>
+                <SetID />
+                <Comments />
+            </Context>
+        </div>
+    );
 }
+
+const SetID = () => {
+    const { setID } = React.useContext(MyContext);
+    // useCallback is used to prevent infinite loop
+    const handleClick = React.useCallback(() => {
+        return setID(Math.floor(Math.random() * 10));
+    }, [setID]);
+    
+    return (
+        <div>
+            <h1>SetID</h1>
+            <button onClick={handleClick}>Set ID</button>
+        </div>
+    );
+};
+
+const Comments = () => {
+    const { id } = React.useContext(MyContext);
+    const [comments, setComments] = React.useState([]);
+    useEffect(() => {
+        console.log('id', id);
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+            .then((res) => res.json())
+            .then((data) => setComments(data));
+    }, [id]);
+    return (
+        <div>
+            <h1>Comments</h1>
+            <ul>
+                {comments.map((comment) => (
+                    <li key={comment.id}>{comment.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
